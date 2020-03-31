c2954285d65174c9828b9aaacc2b38b605aed706
zhaoshanlin <361185955@qq.com>
2020-03-31 10:54:02 +0800

    1

--- src/main/java/SortDemo.java
+++ src/main/java/SortDemo.java
@@ -1,3 +1,21 @@
 public class SortDemo {
 
+    //1冒泡排序
+    public void bubbleSort(int[] a, int n){
+        if(n <= 1) return;
+        for (int i = 0; i < n; i++){
+            //提前退出循环
+            Boolean flag = false;
+            for (int j = 0; j < n-i-1; j++){
+                if(a[j] > a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    flag = true;//表示有数据交换
+                }
+            }
+            if(!flag) break;//表示没有数据交换，停止
+        }
+
+    }
 }
