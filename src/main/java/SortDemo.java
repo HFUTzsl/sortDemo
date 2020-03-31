dc79a24f176aaa6840942c45cea86266deba795a
zhaoshanlin <361185955@qq.com>
2020-03-31 12:23:00 +0800

    1

--- src/main/java/SortDemo.java
+++ src/main/java/SortDemo.java
@@ -16,6 +16,43 @@ public class SortDemo {
             }
             if(!flag) break;//表示没有数据交换，停止
         }
-
     }
+
+
+    //2.插入排序
+    public void insertSort(int[] a,int n){
+        if (n <= 1) return;
+        for (int i = 0 ; i < n; i++){
+            int  j = i - 1;
+            //查找插入的位置
+            for (;j >= 0; j--){
+                if(a[j] > a[i]){
+                    a[j+1] = a[j];//移动数据
+                }else {
+                    break;
+                }
+            }
+            a[j+1] = a[i];
+        }
+    }
+
+    //3.选择排序
+    public void selectionSort(int[] a, int n){
+        if (n <= 1) return;
+
+        for (int i = 0 ; i < n ; i++){
+            int min = i;
+            for (int j = i+1 ; j < n ; j++){
+                if (a[min] > a[j]){
+                    min = j;
+                }
+                int temp = a[min];
+                a[min] = a[j];
+                a[i] = temp;
+            }
+
+        }
+    }
+
+
 }
